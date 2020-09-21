e756f044aec7aa8b9a43713e3b394a3b9d54d6b8
garlicsuter <markasuter@gmail.com>
2020-09-21 09:35:47 -0400

    xRange

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int speed = 10;
+    public float xRange = 9.0f;
+    public float yRange = 6.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        transform.Translate(movement * speed * Time.deltaTime);
+        //if (Input.GetKeyDown(KeyCode.D))
+        //{
+        //    Debug.Log("D was pressed");
+        //    transform.Translate(Vector2.right * speed * Time.deltaTime);
+        //}
+    }
+
+    private void LateUpdate()
+    {
+        //Keep player in bounds of xRange and yRange
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector2(xRange, transform.position.y);
+        }
+    }
+}
--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
+    public int score = 7;
+    public int hiscore = 10;
+    private int temp = 70;
+    public float location;
+    public float loc2 = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello, World!");
+        //Debug.Log("Hello, World!");
+        score += 2;
+        Debug.Log(score + hiscore + temp);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("D was pressed");
+        }
     }
 }
